bbfff5ac8b99c9358afb13926b0565dea98759c2
abdulhayevbahrom <abdulhayevbahromjon2002@gmail.com>
2024-09-13 10:15:55 +0500

    add routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,17 @@
 import React from "react";
 import Header from "./components/header/Header";
+import { Routes, Route } from "react-router-dom";
+import Home from "./routes/home/Home";
+import Heart from "./routes/heart/Heart";
 
 function App() {
   return (
     <div className="app">
       <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/heart" element={<Heart />} />
+      </Routes>
     </div>
   );
 }
