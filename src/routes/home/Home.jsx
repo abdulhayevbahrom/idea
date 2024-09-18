808e8098bd4f3f3b5e6e373c8c7d14d54a1875f5
x9-bayozbek <bayozbekbakromjonov@gmail.com>
2024-09-18 08:10:44 +0500

    ekildi

--- src/routes/home/Home.jsx
+++ src/routes/home/Home.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Banner from "../../components/banner/Banner";
 import Products from "../../components/products/Products";
+import TwoProduct from "../../components/twoProduct/TwoProduct";
 
 function Home() {
   return (
     <div>
       <Banner />
       <Products />
+      <TwoProduct />
     </div>
   );
 }
