485e3159ec0a8c2bdf8015e4a975557cbb89475a
xojiakbar00 <xojiakbarmahmudjanov717@gmail.com>
2024-09-18 09:18:46 +0500

    heart

--- src/routes/heart/Heart.jsx
+++ src/routes/heart/Heart.jsx
@@ -1,9 +1,16 @@
 import React from "react";
+import './Heart.css';import { FaAngleRight } from "react-icons/fa6";
 
 function Heart() {
   return (
-    <div>
-      <h1>SEvimlilar</h1>
+    <div className="heart">
+      <p>Bosh sahifa <span><FaAngleRight /> Tanlanganlar</span></p>
+      <div className="div">
+        <img src="https://idea.uz/_next/image?url=%2Fimg%2Fothers%2Fheart-empty.png&w=1920&q=75" alt="" />
+        <h1>Tanlanganlarda hozircha hech narsa yoʻq</h1>
+        <p>Mahsulot kartochkasida joylashgan yurakchani bosib, mahsulotlarni qoʻshishingiz mumkin</p>
+        <button>Bosh sahifaga o'tish</button>
+      </div>
     </div>
   );
 }
