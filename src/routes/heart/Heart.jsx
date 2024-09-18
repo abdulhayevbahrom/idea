98e651918beef10eb6d1ac3e57758a18eb9711f1
xojiakbar00 <xojiakbarmahmudjanov717@gmail.com>
2024-09-18 22:12:11 +0500

    heart

--- src/routes/heart/Heart.jsx
+++ src/routes/heart/Heart.jsx
@@ -1,7 +1,10 @@
 import React from "react";
 import "./Heart.css";
 import { FaAngleRight } from "react-icons/fa6";
-import { useNavigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import { GoTrash } from "react-icons/go";
+import { LuShoppingBag } from "react-icons/lu";
+import { LiaBalanceScaleSolid } from "react-icons/lia";
 
 function Heart() {
   const navigate = useNavigate();
@@ -31,7 +34,31 @@ function Heart() {
         </div>
       ) : (
         <div className="heart_content">
-          <h1>malumotlar</h1>
+          <div className="tanlanganlar">
+            <div className="tanlangan">
+              <img src="https://idea.uz/_next/image?url=https%3A%2F%2Fapi.idea.uz%2Fstorage%2Fproducts%2FDecember2021%2FpThb4rqQNdNZF1AyE9Fa.jpg&w=128&q=75" alt="" />
+              <div className="lorem">
+                <div className="som">
+                  <h2>1 165 000 soʻm</h2>
+                  <span> х 6 oy</span>
+                </div>
+                <Link to={"/"} className="link">LG F2M5HS6S kir yuvish mashinasi</Link>
+                <span>Mahsulot kodi: 4688</span>
+              </div>
+              <GoTrash />
+              <LuShoppingBag />
+              <LiaBalanceScaleSolid />
+            </div>
+          </div>
+          <div className="sizning_istaklaringiz">
+            <h3>Sizning istaklaringiz roʻyxati</h3>
+            <div className="soni">
+              <span>Mahsulotlar soni</span>
+              <span>0</span>
+            </div>
+            <button className="btn1">Hammasini o'chirish</button>
+            <button className="btn2">Savatga o'tish</button>
+          </div>
         </div>
       )}
     </div>
