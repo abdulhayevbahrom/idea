3717662fb847ff79367ae4cf7f66f49a1b0b5a63
x9-bayozbek <bayozbekbakromjonov@gmail.com>
2024-09-30 08:13:47 +0500

    ekildi

--- src/routes/search/Search.jsx
+++ src/routes/search/Search.jsx
@@ -1,10 +1,14 @@
 import React from "react";
 import "./Search.css";
 import { Link, useParams } from "react-router-dom";
-import { FaChevronRight } from "react-icons/fa";
+import { FaChevronRight, FaChevronUp } from "react-icons/fa";
 import { IoGrid } from "react-icons/io5";
 import { CiGrid2H } from "react-icons/ci";
 import { smartPhones } from "../../data/products";
+import Product from "../../components/products/Products"
+import "swiper/css";
+import "swiper/css/pagination";
+import "swiper/css/navigation";
 
 function Search() {
   const { name } = useParams();
@@ -19,7 +23,16 @@ function Search() {
       </div>
       <h1 className="searchTitle">{name}</h1>
       <div className="searchWrapper">
-        <div className="search_sidebar"></div>
+        <div className="search_sidebar">
+          <div className="topSidebar">
+            <input type="text" placeholder="telefon" />
+          </div>
+          <div className="bottomSidebar">
+            <h3>Ishlab chiqaruvch <FaChevronUp /></h3>
+            <input type="text" placeholder="Ishlab chiqaruvchini topish"/>
+            <input type="checkBox" />
+          </div>
+        </div>
         <div className="search_result">
           <div className="result_filter">
             <div className="result_filter_btns">
@@ -33,6 +46,9 @@ function Search() {
               <CiGrid2H />
             </div>
           </div>
+      
+
+          <Product data={smartPhones} slidesPerView={4}  />
           <div className="result_wrapper"></div>
         </div>
       </div>
